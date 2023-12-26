ec4bf11d3fc18b13f536b3146cd569eedee2c0da
sssenya <senia7@bk.ru>
2023-12-26 12:29:43 +0300

    Добавлена команда во ViewModel

--- RevitRoomFinishing/ViewModels/MainViewModel.cs
+++ RevitRoomFinishing/ViewModels/MainViewModel.cs
@@ -25,7 +25,6 @@ namespace RevitRoomFinishing.ViewModels {
         private ObservableCollection<ElementsGroup> _ceilingTypes;
 
         private string _errorText;
-        private string _saveProperty;
 
         public MainViewModel(PluginConfig pluginConfig, RevitRepository revitRepository) {
             _pluginConfig = pluginConfig;
@@ -36,11 +35,14 @@ namespace RevitRoomFinishing.ViewModels {
             _floorTypes = _revitRepository.GetFinishingTypes(BuiltInCategory.OST_Floors);
             _ceilingTypes = _revitRepository.GetFinishingTypes(BuiltInCategory.OST_Ceilings);
 
-            LoadViewCommand = RelayCommand.Create(LoadView);
-            AcceptViewCommand = RelayCommand.Create(AcceptView, CanAcceptView);
+            SelectedPhase = _phases[_phases.Count - 1];
+
+            CalculateFinishingCommand = RelayCommand.Create(CalculateFinishing, CanCalculateFinishing);
         }
 
         public List<Phase> Phases => _phases;
+
+        public ICommand CalculateFinishingCommand { get; }
 
         public Phase SelectedPhase {
             get => _selectedPhase;
@@ -70,31 +72,13 @@ namespace RevitRoomFinishing.ViewModels {
             set => this.RaiseAndSetIfChanged(ref _ceilingTypes, value);
         }
 
+        private void CalculateFinishing() {
 
-        public ICommand LoadViewCommand { get; }
-        public ICommand AcceptViewCommand { get; }
-
-        public string ErrorText {
-            get => _errorText;
-            set => this.RaiseAndSetIfChanged(ref _errorText, value);
         }
 
-        public string SaveProperty {
-            get => _saveProperty;
-            set => this.RaiseAndSetIfChanged(ref _saveProperty, value);
-        }
-
-        private void LoadView() {
-            LoadConfig();
-        }
-
-        private void AcceptView() {
-            SaveConfig();
-        }
-        
-        private bool CanAcceptView() {
-            if(string.IsNullOrEmpty(SaveProperty)) {
-                ErrorText = "Введите значение сохраняемого свойства.";
+        private bool CanCalculateFinishing() {
+            if(Rooms.Count == 0) {
+                ErrorText = "Помещения отсутствуют на выбранной стадии";
                 return false;
             }
 
@@ -102,18 +86,9 @@ namespace RevitRoomFinishing.ViewModels {
             return true;
         }
 
-        private void LoadConfig() {
-            RevitSettings setting = _pluginConfig.GetSettings(_revitRepository.Document);
-
-            SaveProperty = setting?.SaveProperty ?? "Привет Revit!";
-        }
-
-        private void SaveConfig() {
-            RevitSettings setting = _pluginConfig.GetSettings(_revitRepository.Document)
-                                    ?? _pluginConfig.AddSettings(_revitRepository.Document);
-
-            setting.SaveProperty = SaveProperty;
-            _pluginConfig.SaveProjectConfig();
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
         }
     }
 }
