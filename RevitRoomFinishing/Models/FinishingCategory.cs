86d1a78b96c070abe86c4a80614f7a2d8d227275
sssenya <senia7@bk.ru>
2024-01-22 11:48:59 +0300

    Создан класс для хранения информации о категориях отделки

---
+++ RevitRoomFinishing/Models/FinishingCategory.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace RevitRoomFinishing.Models {
+    internal class FinishingCategory {
+        public string Name { get; set; }
+        public string KeyWord { get; set; }
+        public BuiltInCategory Category { get; set; }
+
+        public static FinishingCategory WallsFinishing { get; } = new FinishingCategory() {
+            Name = "Стены",
+            KeyWord = "(О) Стена",
+            Category = BuiltInCategory.OST_Walls
+        };
+
+        public static FinishingCategory BaseboardsFinishing { get; } = new FinishingCategory() {
+            Name = "Плинтусы",
+            KeyWord = "(О) Плинтус",
+            Category = BuiltInCategory.OST_Walls
+        };
+
+        public static FinishingCategory FloorsFinishing { get; } = new FinishingCategory() {
+            Name = "Перекрытия",
+            KeyWord = "(АР)",
+            Category = BuiltInCategory.OST_Walls
+        };
+
+        public static FinishingCategory CeilingsFinishing { get; } = new FinishingCategory() {
+            Name = "Потолки",
+            KeyWord = "(О) Потолок",
+            Category = BuiltInCategory.OST_Walls
+        };
+
+    }
+}
